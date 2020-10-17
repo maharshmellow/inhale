36ec131c34b45927f8eff9fc367e92640a47674b
Maharsh Patel <patel@maharsh.net>
2020-10-17 00:29:10 -0600

    add timer countdown on click

--- Breathe/Breathe/ContentView.swift
+++ Breathe/Breathe/ContentView.swift
@@ -8,16 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var started = false
     @State private var circleVar = false
+    @State var timeRemaining = 10
+    
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    
+    let impactLight = UIImpactFeedbackGenerator(style: .light)
     
     var body: some View {
         VStack {
             Text("exhale")
                 .padding(.top, 60)
-                .font(.system(size: 26, weight: .medium))
-            Text("4")
-                .padding(.top, 60)
-                .font(.system(size: 32, weight: .medium))
+                .font(.system(size: 28, weight: .medium))
+            Text("\(timeRemaining)")
+                .padding(.top, 100)
+                .font(.system(size: 32, weight: .semibold))
             Spacer()
             Circle()
                 .frame(width: 150, height: 150, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
@@ -26,15 +32,26 @@ struct ContentView: View {
                 .onTapGesture {
                     print("Tapped")
                     
-                    let impactLight = UIImpactFeedbackGenerator(style: .light)
+                    
                     impactLight.impactOccurred()
                     
                     circleVar.toggle()
+                    started.toggle()
                 }
             Spacer()
             Text("3")
                 .foregroundColor(.gray)
-                .font(.system(size: 32, weight: .medium))
+                .font(.system(size: 32, weight: .semibold))
+        }.onReceive(timer) { _ in
+            if !self.started {
+                return
+            }
+            
+            if self.timeRemaining > 0 {
+                self.timeRemaining -= 1
+                impactLight.impactOccurred()
+            }
+            // add some logic here for switching between exhale/inhale/hold
         }
     }
 }
