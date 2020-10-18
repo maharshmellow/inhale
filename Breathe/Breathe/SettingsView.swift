27664c4667f897a392f27cf457bb4f8bf2528190
Maharsh Patel <patel@maharsh.net>
2020-10-18 01:11:51 -0600

    add blank settings format

--- Breathe/Breathe/SettingsView.swift
+++ Breathe/Breathe/SettingsView.swift
@@ -12,9 +12,31 @@ struct SettingsView: View {
     
     var body: some View {
         NavigationView {
-            VStack {
-                Text("Sheet View content")
-            }
+            List {
+                Section {
+                    NavigationLink(destination: Text("Detail View")) {
+                        HStack {
+                            ZStack {
+                                Image(systemName: "circle")
+                                    .foregroundColor(.white)
+                                    .font(.callout)
+                            }.frame(width: 28, height: 28).background(Color.orange).cornerRadius(6)
+                            Text("Breathing Pattern")
+                        }
+                    }
+                    NavigationLink(destination: Text("Detail View")) {
+                        HStack {
+                            ZStack {
+                                Image(systemName: "airplane")
+                                    .foregroundColor(.white)
+                                    .font(.callout)
+                            }.frame(width: 28, height: 28).background(Color.blue).cornerRadius(6)
+                            Text("Theme")
+                        }
+                    }
+                }
+            }.listStyle(GroupedListStyle())
+            .navigationTitle("Settings")
             .navigationBarItems(trailing: Button(action: {
                 self.showSettingsView = false
             }) {
